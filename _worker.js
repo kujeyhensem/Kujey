093fa5724f1e43c7a240f58728a26ae76a6baba5
kujeyhensem <98367149+kujeyhensem@users.noreply.github.com>
2025-05-08 04:12:22 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,9 +5,9 @@ import { connect } from "cloudflare:sockets";
 // Variables
 const rootDomain = "gampangdiinget7.workers.dev"; // Ganti dengan domain utama kalian
 const serviceName = "kujey"; // Ganti dengan nama workers kalian
-const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
-const apiEmail = ""; // Ganti dengan email yang kalian gunakan
-const accountID = ""; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
+const apiKey = "5820ac08eae44af332296609e17864bdd6034"; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
+const apiEmail = "gampangdiinget7@gmail.com"; // Ganti dengan email yang kalian gunakan
+const accountID = "471d99c0479aff57fc8da80f85cffbec"; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
 const zoneID = ""; // Ganti dengan Zone ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
 let isApiReady = false;
 let proxyIP = "";
